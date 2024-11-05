d244eb09beaf9b73a793af48ba23b243b9ff9f96
Somkumar <somkumarav@gmail.com>
2024-11-06 02:10:37 +0530

    style: reduce margin bottom in section title

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export const Section = (props: { heading: string; children: ReactNode }) => {
   return (
     <div>
-      <p className='mb-6'>{props.heading}</p>
+      <p className='mb-4'>{props.heading}</p>
       <div className='text-neutral-400'>{props.children}</div>
     </div>
   );
