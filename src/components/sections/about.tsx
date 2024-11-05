3882950fb295584f5706ca53581fead7a08818d9
Somkumar <somkumarav@gmail.com>
2024-11-06 02:21:47 +0530

    fix: static 5 years to dynamic year based on current date

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,11 +1,13 @@
 import { Section } from "../section";
 
 export const AboutSection = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Section title='About'>
       <p>
-        I&apos;ve spent the last years building software. TypeScript and React
-        are my weapons of choice.
+        I&apos;ve spent the last {currentYear - 2019} years building software.
+        TypeScript and React are my weapons of choice.
       </p>
     </Section>
   );
