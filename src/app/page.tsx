0fcbb33cba2cfaf93b85a928b87317f59edba86d
Somkumar <somkumarav@gmail.com>
2024-11-06 02:20:39 +0530

    refactor: about section code to its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import { LinkCTA } from "../components/link";
 import { MyInfo } from "../components/my-info";
 import { Section } from "../components/section";
+import { AboutSection } from "../components/sections/about";
 
 export default function Home() {
   return (
     <div className='space-y-12 mt-12'>
       <MyInfo />
-      <Section title='About'>
-        <p>
-          I&apos;ve spent the last 5 years building software. TypeScript and
-          React are my weapons of choice.
-        </p>
-      </Section>
+      <AboutSection />
 
       <Section title='Connect'>
         <p>
---
+++ src/components/sections/about.tsx
@@ -0,0 +1,12 @@
+import { Section } from "../section";
+
+export const AboutSection = () => {
+  return (
+    <Section title='About'>
+      <p>
+        I&apos;ve spent the last years building software. TypeScript and React
+        are my weapons of choice.
+      </p>
+    </Section>
+  );
+};
