a4a9f973952433cc56624cafa0e6b1cd8e9d9028
Somkumar <somkumarav@gmail.com>
2024-11-06 19:41:53 +0530

    impr: add hover state to link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -5,7 +5,7 @@ export const LinkCTA = (props: { children: ReactNode; href: string }) => {
   return (
     <Link
       href={props.href}
-      className='text-foreground underline'
+      className='text-foreground underline hover:text-sky-300 transition-colors'
       target='_blank'
     >
       {props.children}
