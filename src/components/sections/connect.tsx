fa7617efd109f06672ca52db4440f53a805387a5
Somkumar <somkumarav@gmail.com>
2024-11-06 15:14:48 +0530

    fix: add space after comma

--- src/components/sections/connect.tsx
+++ src/components/sections/connect.tsx
@@ -5,8 +5,8 @@ export const ConnectSection = () => {
   return (
     <Section title='Connect'>
       <p>
-        Follow me on <LinkCTA href='https://x.com/somkumar_va'>X</LinkCTA>
-        ,view my code and open-source projects on{" "}
+        Follow me on <LinkCTA href='https://x.com/somkumar_va'>X</LinkCTA>, view
+        my code and open-source projects on{" "}
         <LinkCTA href='https://github.com/somkumarav'>Github</LinkCTA>, or email
         me directly{" "}
         <LinkCTA href='mailto:somkumarav@gmail.com'>
