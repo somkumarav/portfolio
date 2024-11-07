3bbe72536449ba389475e09a9dced816f5830e4d
Somkumar <somkumarav@gmail.com>
2024-11-07 13:16:57 +0530

    add: project to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { MyInfo } from "../components/my-info";
 import { AboutSection } from "../components/sections/about";
 import { ConnectSection } from "../components/sections/connect";
+import { ProjectSection } from "../components/sections/projects";
 
 export default function Home() {
   return (
     <div className='space-y-12 mt-16'>
       <MyInfo />
       <AboutSection />
+      <ProjectSection />
       <ConnectSection />
     </div>
   );
---
+++ src/components/sections/projects.tsx
@@ -0,0 +1,22 @@
+import { Project } from "../project";
+import { Section } from "../section";
+
+export const ProjectSection = () => {
+  return (
+    <Section title='Projects'>
+      <Project
+        title='Cloud photo share'
+        tags={["React", "Next.js", "Typescript", "AWS", "CloudFlare R2"]}
+      >
+        Ever taken a bunch of amazing photos on your last trip, wedding or
+        family gathering? You probably snapped a tone of photos, right? Tired of
+        the hassle of emailing huge files or sharing them on social media? Well,
+        I got you. Using this easy-to-use app, lets you create beautiful photo
+        albums and share them with your loved ones. Just upload your pics, and
+        share a special link. Your friends and family can then view, download,
+        and even comment on your photos - all without any complicated sign-ups
+        or downloads.
+      </Project>
+    </Section>
+  );
+};
