28ffd1ea91cb8e0a3b86cbeff16d5600aa7f4f9f
Somkumar <somkumarav@gmail.com>
2024-11-07 13:59:26 +0530

    impr: add underline flag to linkCTA component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import { cn } from "../../lib/cn";
 
-export const LinkCTA = (props: { children: ReactNode; href: string }) => {
+export const LinkCTA = ({
+  removeUnderLine = false,
+  ...props
+}: {
+  children: ReactNode;
+  href: string;
+  removeUnderLine?: boolean;
+}) => {
   return (
     <Link
       href={props.href}
-      className='text-foreground underline hover:text-sky-300 transition-colors'
+      className={cn(
+        "text-foreground hover:text-sky-300 transition-colors",
+        !removeUnderLine ? "underline" : ""
+      )}
       target='_blank'
     >
       {props.children}
