d19c8dd0aff167791e4264174eb187c989b5a9b7
somkumarav <somkumarav@gmail.com>
2025-10-13 22:18:51 +0100

    style: some padding/margin changes and change in favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { ProjectSection } from "../components/sections/projects";
 
 export default function Home() {
   return (
-    <div className='space-y-12 mt-12 mb-2'>
+    <div className='space-y-10 mt-12 mb-2'>
       <MyInfo />
       <AboutSection />
       <ProjectSection />
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export const Section = (props: { title: string; children: ReactNode }) => {
   return (
     <div>
-      <p className='mb-4'>{props.title}</p>
+      <p className='mb-4 text-lg'>{props.title}</p>
       <div className='text-neutral-400'>{props.children}</div>
     </div>
   );
