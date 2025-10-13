4f7d62a58afe66097e29887c0ef21ce2ff0c4de7
somu <somkumarav@gmail.com>
2025-10-13 10:26:51 +0100

    revert: all deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/nextjs-github-pages",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
