ff437fbba275bac35fc5e8e6d68f3458f465a953
somkumarav <somkumarav@gmail.com>
2025-11-16 23:17:26 +0000

    other: change folder structure **major**

--- components/link.tsx
+++ components/link.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-import { cn } from "../../lib/cn";
+import { cn } from "../lib/cn";
 
 export const LinkCTA = ({
   removeUnderLine = false,
