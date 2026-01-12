54723d23f66dd310c65d7da6e310d822ea3699d9
somkumarav <somkumarav@gmail.com>
2026-01-12 03:25:19 +0000

    other: migration from pnpm to bun

--- src/components/icons/web-logo.tsx
+++ src/components/icons/web-logo.tsx
@@ -7,7 +7,7 @@ export const WebLogo = () => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
+      strokeWidth='2'
       strokeLinecap='round'
       strokeLinejoin='round'
       className='lucide lucide-link-icon lucide-link'
